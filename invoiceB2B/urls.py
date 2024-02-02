5158ea06d5cbde6b31062d64b605f3f5bef430ff
shanmugapriyadev <108174009+shanmugapriyadev@users.noreply.github.com>
2024-02-02 15:47:49 +0530

    Update urls.py

--- invoiceB2B/urls.py
+++ invoiceB2B/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/core/', include('core.urls')),
     path('api/company/', include('company.urls')),
     path('api/invoice/', include('invoice.urls')),
